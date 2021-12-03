52c85a70e339414219f30b161ccd22b563e1205b
0xda203 <gabloon1@tuta.io>
2021-12-03 04:24:18 -0300

    tratamento de erros melhorados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
+const cc = require("node-console-colors");
 const Parser = require('./components/Parser')
-const { Lexer, TOKENS}  = require('./components/Lexer')
+const { Lexer }  = require('./components/Lexer')
 
 const compile = async () => {
 	// TODO aprimorar CLI
@@ -8,12 +9,12 @@ const compile = async () => {
 	const path = args[0]
 	try {
 		const data = fs.readFileSync(path, 'utf-8')
-		const parser = Parser(Lexer, TOKENS);
+		const parser = Parser(Lexer);
 		const AST = parser.parse(data);
 		console.log(AST.toString())
 	} catch (err) {
-		if (err.code === 'ENOENT') console.error('Erro: Arquivo de entrada inválido')
-		else console.error(err)
+		if (err.code === 'ENOENT') console.error(cc.set("fg_dark_red", 'Erro: Arquivo de entrada inválido.'))
+		else console.error(cc.set("fg_dark_red", err.name + ": " + err.message))
 	}
 }
 
