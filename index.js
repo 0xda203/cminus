1b50c4aa7ae4e7a3ef0bfc2b4f240a0e9bc32fa9
0xda203 <gabloon1@tuta.io>
2021-12-22 17:45:26 -0300

    Função para exibição de erros no console adicionada

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const Parser = require('./components/Parser')
 const { Lexer }  = require('./components/Lexer')
 
 const compile = async () => {
-	// TODO aprimorar CLI
 	const [, , ...args] = process.argv
 	const path = args[0]
 	try {
 		const data = fs.readFileSync(path, 'utf-8')
+		const lines = data.toString().split('\n')
+
+		const showError = require('./util/errors')(lines);
+
 		const parser = Parser(Lexer);
-		const AST = parser.parse(data);
-		console.log(AST.toString())
+		const ast = parser.parse(data);
 	} catch (err) {
 		if (err.code === 'ENOENT') console.error(cc.set("fg_dark_red", 'Erro: Arquivo de entrada inválido.'))
 		else console.error(cc.set("fg_dark_red", err.name + ": " + err.message))
