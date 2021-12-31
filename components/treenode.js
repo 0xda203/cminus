085940072afb2010da4c7596cbb16d8d1a1021d8
0xda203 <gabloon1@tuta.io>
2021-12-30 22:32:51 -0300

    torna propriedade name enumerável

--- components/treenode.js
+++ components/treenode.js
@@ -7,37 +7,39 @@ function TreeNode(lineno, name, children = [], attr = {}) {
     }
 
     Object.defineProperty(obj, 'name', {
-        enumerable : false,
-        value : name
+        enumerable: true,
+        value: name
     })
 
     Object.defineProperty(obj, 'lineno', {
-        enumerable : false,
-        value : lineno
+        enumerable: false,
+        value: lineno
     })
 
     if (children.length > 0) {
         children = children.filter(child => child != null);
-        // obj[name] = {...obj[name], ..._children}
-        Object.defineProperty(obj[name], 'children', {
-            enumerable : true,
-            value : children
-        });
-        
+        // obj[name] = {...obj[name], ..._children}        
     }
+
+    Object.defineProperty(obj[name], 'children', {
+        enumerable: true,
+        value: children
+    });
+
     if (Object.keys(attr).length > 0) {
-        obj[name] = {...obj[name], ...attr};
-        Object.defineProperty(obj[name], 'attr', {
-            enumerable : false,
-            value : attr
-        });
+        obj[name] = { ...obj[name], ...attr };
     }
+
+    Object.defineProperty(obj[name], 'attr', {
+        enumerable: false,
+        value: attr
+    });
 
     obj.toString = function () {
         return treeify.asTree(obj, true, true);
     }
-    
+
     return obj;
-}   
+}
 
 module.exports = TreeNode;
