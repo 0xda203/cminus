23ab61bdd2aa97f9a067f7518dd788ef6a291c3b
0xda203 <gabloon1@tuta.io>
2022-01-06 10:33:19 -0300

    corrigido erro quando elemento não era encontrado

--- util/errors.js
+++ util/errors.js
@@ -1,13 +1,14 @@
 const cc = require("node-console-colors");
 
-module.exports = function(lines) {
-    function showError(elem, lineno, err, errname = "SemanticError") {
+module.exports = function (lines) {
+    function showError(elem, lineno, err, print = false, errname = "SemanticError",) {
         let index = lines[lineno].indexOf(elem);
         const start = ((index - 18) > 0) ? index - 18 : 0;
         const end = ((index + 18) > lines[lineno].length) ? index + 18 : lines[lineno].length;
-        let substr = (start === 0 ? "" : "...") + lines[lineno].substring(start, end) + "\n" + 
-            "-".repeat(start === 0 ? index  : 21) + "^";
-        console.error(cc.set("fg_dark_red", errname + ": " + err + "\n" + substr));
+        let substr = (start === 0 ? "" : "...") + lines[lineno].substring(start, end) + "\n" +
+            "-".repeat(start === 0 ? index < 0 ? 1 : index : 21) + "^" + "\n";
+        if (print) return console.error(cc.set("fg_dark_red", errname + ": ") + cc.set("fg_white", err + "\n" + substr));
+        else return (errname === "" ? "" : ": ") + err + "\n" + substr;
     }
     return showError;
 }
