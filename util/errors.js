e23e91c06c59b825f3914d674d0bf36c4299ad6f
0xda203 <gabloon1@tuta.io>
2022-01-11 15:10:50 -0300

    atualização no README

--- util/errors.js
+++ util/errors.js
@@ -1,7 +1,7 @@
 const cc = require("node-console-colors");
 
-module.exports = function (lines) {
-    function showError(elem, lineno, err, print = false, errname = "SemanticError",) {
+module.exports = (lines) => {
+    return (elem, lineno, err, print = false, errname = "SemanticError") => {
         let index = lines[lineno].indexOf(elem);
         const start = ((index - 18) > 0) ? index - 18 : 0;
         const end = ((index + 18) > lines[lineno].length) ? index + 18 : lines[lineno].length;
@@ -9,6 +9,5 @@ module.exports = function (lines) {
             "-".repeat(start === 0 ? index < 0 ? 1 : index : 21) + "^" + "\n";
         if (print) return console.error(cc.set("fg_dark_red", errname + ": ") + cc.set("fg_white", err + "\n" + substr));
         else return (errname === "" ? "" : ": ") + err + "\n" + substr;
-    }
-    return showError;
+    };
 }
