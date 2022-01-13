2b6ed0cab89ce903ae6c9218194c46bde20b197d
0xda203 <gabloon1@tuta.io>
2022-01-13 05:45:26 -0300

    alteração do código para seguir o padrão de construção da árvore

--- util/io.js
+++ util/io.js
@@ -4,16 +4,16 @@ module.exports = function (sibling) {
     let input = new TreeNode(-1, 'FUNC_DECL', [
         new TreeNode(-1, 'TYPE', [], { type: 'int' }),
         // new TreeNode(-1, 'COMPOUND_STMT', []),
-    ], { name: "input" })
+    ], { identifier: "input" })
 
     let output = new TreeNode(-1, 'FUNC_DECL', [
         new TreeNode(-1, 'TYPE', [], { type: 'void' }),
         new TreeNode(-1, 'PARAM_VAR', [
             new TreeNode(-1, 'TYPE', [], { type: 'int' }),
-            new TreeNode(-1, 'IDENTIFIER', [], { name: 'x' })
+            new TreeNode(-1, 'IDENTIFIER', [], { identifier: 'x' })
         ]),
         // new TreeNode(-1, 'COMPOUND_STMT', [])
-    ], { name: "output" });
+    ], { identifier: "output" });
 
     input.isNative = true;
     output.isNative = true;
